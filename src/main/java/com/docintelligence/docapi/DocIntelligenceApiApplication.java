53ddbf3354a9e168bc608d745c2a54ff8c9b3865
Debasis Rout <drout17@gmail.com>
2026-05-19 14:03:42 +0530

    feat: initial project setup with Spring Boot 3.5 + Java 21 + pgvector

---
+++ src/main/java/com/docintelligence/docapi/DocIntelligenceApiApplication.java
@@ -0,0 +1,13 @@
+package com.docintelligence.docapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocIntelligenceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocIntelligenceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docintelligence/docapi/DocIntelligenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docintelligence.docapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocIntelligenceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
